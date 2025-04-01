d3b21c2e17519e926e8e01ca4e3b76a4383f77c4
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-04-01 01:02:08 -0400

    Added background color generated dynamically

--- src/components/ColorGenerator.jsx
+++ src/components/ColorGenerator.jsx
@@ -1,9 +1,18 @@
 import '../css/ColorGenerator.css';
+import { useState } from 'react';
 
 function ColorGenerator() {
-    return (
-        <div>
+    const [typeOfColor, setTypeOfColor] = useState('hex');
+    const [color, setColor] = useState('#000000');
 
+    return (
+        <div 
+            className="cg-container" 
+            style={{background: color}}
+        >
+            <button>Create HEX Color</button>
+            <button>Create RGB Color</button>
+            <button>Generate Random Color</button>
         </div>
     )
 }
