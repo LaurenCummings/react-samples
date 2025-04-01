18dbb63179a967f7f71db07c66f0161b3236ec58
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-04-01 01:08:59 -0400

    Added onClick handlers for buttons

--- src/components/ColorGenerator.jsx
+++ src/components/ColorGenerator.jsx
@@ -5,14 +5,24 @@ function ColorGenerator() {
     const [typeOfColor, setTypeOfColor] = useState('hex');
     const [color, setColor] = useState('#000000');
 
+    function createRandomColor() {
+        
+    }
+
     return (
         <div 
             className="cg-container" 
             style={{background: color}}
         >
-            <button>Create HEX Color</button>
-            <button>Create RGB Color</button>
-            <button>Generate Random Color</button>
+            <button onClick={() => setTypeOfColor('hex')}>
+                Create HEX Color
+            </button>
+            <button onClick={() => setTypeOfColor('rgb')}>
+                Create RGB Color
+            </button>
+            <button onClick={createRandomColor}>
+                Generate Random Color
+            </button>
         </div>
     )
 }
