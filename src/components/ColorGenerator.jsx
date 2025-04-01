c97c7e0cd87d006cb40a05c3adfaaeca81e4168c
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-04-01 01:21:04 -0400

    Added function to generate random hex color

--- src/components/ColorGenerator.jsx
+++ src/components/ColorGenerator.jsx
@@ -6,7 +6,17 @@ function ColorGenerator() {
     const [color, setColor] = useState('#000000');
 
     function createRandomColor() {
-        
+        if (typeOfColor === 'hex') {
+            const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+            let hexColor = "#";
+
+            for (let i = 0; i < 6; i++) {
+                hexColor += hex[Math.floor(Math.random() * hex.length)];
+            }
+            setColor(hexColor);
+        } else {
+
+        }
     }
 
     return (
