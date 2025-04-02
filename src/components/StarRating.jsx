ee509b6c1d3f168da02f43e550a48813c71e119a
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-04-02 00:28:05 -0400

    Added structure of component

--- src/components/StarRating.jsx
+++ src/components/StarRating.jsx
@@ -1,9 +1,37 @@
 import '../css/StarRating.css';
+import { useState } from 'react';
+import { FaStar } from 'react-icons/fa';
 
-function StarRating() {
+function StarRating(numOfStars = 5) {
+    const [rating, setRating] = useState(0);
+    const [hover, setHover] = useState(0);
+
+    function handleClick(getCurrentIndex) {
+
+    }
+
+    function handleMouseEnter(getCurrentIndex) {
+
+    }
+
+    function handleMouseLeave(getCurrentIndex) {
+
+    }
+
     return (
-        <div>
+        <div className="star-rating">
+            {[...Array(numOfStars)].map((_,index) => {
+                index += 1
 
+                return 
+                    <FaStar 
+                        key={index}
+                        onClick={() => handleClick(index)}
+                        onMouseMove={() => handleMouseEnter(index)}
+                        onMouseLeave={() => handleMouseLeave(index)}
+                        size={40}
+                    />
+            })}
         </div>
     )
 }
