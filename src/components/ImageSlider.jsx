d8371a90ec8b27fb8bbef43aaf817e9baaca23eb
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-04-03 01:51:48 -0400

    Added slider logic and indicator styles and logic

--- src/components/ImageSlider.jsx
+++ src/components/ImageSlider.jsx
@@ -25,6 +25,14 @@ function ImageSlider({url, limit, page}) {
         }
     }
 
+    function handlePrevious() {
+        setCurrentSlide(currentSlide === 0 ? images.length - 1 : currentSlide - 1);
+    }
+
+    function handleNext() {
+        setCurrentSlide(currentSlide === images.length - 1 ? 0 : currentSlide + 1);
+    }
+
     useEffect(() => {
         if (url !== '') fetchImages(url)
     }, [url]);
@@ -41,25 +49,35 @@ function ImageSlider({url, limit, page}) {
 
     return (
         <div className="image-slider">
-            <BsArrowLeftCircleFill className="arrow arrow-left" />
+            <BsArrowLeftCircleFill 
+                onClick={handlePrevious} 
+                className="arrow arrow-left" 
+            />
             {images && images.length 
-                ? images.map(imageItem => (
+                ? images.map((imageItem, index) => (
                     <img
                         key={imageItem.id}
                         alt={imageItem.download_url}
                         src={imageItem.download_url}
-                        className="current-image"
+                        className={currentSlide === index ? "current-image" : "hide-image"}
                     />
                 )) : null
             }
-            <BsArrowRightCircleFill className="arrow arrow-right" />
+            <BsArrowRightCircleFill 
+                onClick={handleNext} 
+                className="arrow arrow-right" 
+            />
             <span className="circle-indicators">
                 {
                     images && images.length ? 
                     images.map((_, index) => 
                         <button
                             key={index}
-                            className="current-indicator">
+                            className={
+                                currentSlide === index 
+                                    ? "indicator active-indicator" 
+                                    : "indicator non-active-indicator"}
+                            onClick={() => setCurrentSlide(index)}>
                         </button>
                     )
                     : null
