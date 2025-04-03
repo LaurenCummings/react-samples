462fc722e86075755b36c9d6529fa77013e0eb1d
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-04-03 00:04:32 -0400

    Created ImageSlider component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import './css/App.css';
 import Accordian from './components/Accordian';
 import ColorGenerator from './components/ColorGenerator';
 import StarRating from './components/StarRating';
+import ImageSlider from './components/ImageSlider';
 
 function App() {
 
   return (
     <div>
-      <StarRating numOfStars={10} />
+      <ImageSlider />
     </div>
   )
 }
---
+++ src/components/ImageSlider.jsx
@@ -0,0 +1,11 @@
+import '../css/ImageSlider.css';
+
+function ImageSlider() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default ImageSlider;
