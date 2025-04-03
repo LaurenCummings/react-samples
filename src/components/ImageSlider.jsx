e67b255874b52874726fd77d21d8e6f6e03492be
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-04-03 00:11:18 -0400

    Checked for url and wrote function to fetch images

--- src/components/ImageSlider.jsx
+++ src/components/ImageSlider.jsx
@@ -1,8 +1,35 @@
 import '../css/ImageSlider.css';
+import { useState, useEffect } from 'react';
 
-function ImageSlider() {
+function ImageSlider(url, limit) {
+    const [images, setImages] = useState([]);
+    const [currentSlide, setCurrentSlide] = useState(0);
+    const [errorMsg, setErrorMsg] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    async function fetchImages(url) {
+        try {
+            setLoading(true);
+
+            const response = await fetch(url);
+            const data = await response.json();
+
+            if (data) {
+                setImages(data);
+                setLoading(false);
+            }
+        } catch(e) {
+            setErrorMsg(e.message);
+            setLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        if (url !== '') fetchImages(url)
+    }, [url])
+
     return (
-        <div>
+        <div className="image-slider">
 
         </div>
     )
