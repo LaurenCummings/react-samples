533f254c69a294e96c716a0966e2fef6b9280bda
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-04-04 00:48:57 -0400

    Created LoadMoreData component

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,13 @@ import Accordian from './components/Accordian';
 import ColorGenerator from './components/ColorGenerator';
 import StarRating from './components/StarRating';
 import ImageSlider from './components/ImageSlider';
+import LoadMoreData from './components/LoadMoreData';
 
 function App() {
 
   return (
     <div>
-      <ImageSlider 
-        url={"https://picsum.photos/v2/list"} 
-        page={"1"} 
-        limit={"10"} 
-      />
+      <LoadMoreData />
     </div>
   )
 }
---
+++ src/components/LoadMoreData.jsx
@@ -0,0 +1,11 @@
+import '../css/LoadMoreData.css';
+
+function LoadMoreData() {
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default LoadMoreData;
