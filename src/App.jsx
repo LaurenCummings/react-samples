f76427e7155af33eb99803f7649efe82587f3239
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-04-05 00:28:00 -0400

    Created TreeMenu component

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,13 @@ import ColorGenerator from './components/ColorGenerator';
 import StarRating from './components/StarRating';
 import ImageSlider from './components/ImageSlider';
 import LoadMoreData from './components/LoadMoreData';
+import TreeMenu from './components/TreeMenu';
 
 function App() {
 
   return (
     <div>
-      <LoadMoreData />
+      <TreeMenu />
     </div>
   )
 }
---
+++ src/components/TreeMenu.jsx
@@ -0,0 +1,11 @@
+import '../css/TreeMenu.css';
+
+function TreeMenu() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default TreeMenu;
