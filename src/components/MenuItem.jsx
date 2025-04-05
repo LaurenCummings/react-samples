4f2360c2f63556e72c361d7bfc5bc77a6f8d1328
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-04-05 01:19:04 -0400

    Added recursive call to MenuList inside MenuItem

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -1,9 +1,16 @@
 import '../css/TreeMenu.css';
+import MenuList from '../components/MenuList';
 
 function MenuItem({item}) {
     return (
         <li>
             <p>{item.label}</p>
+
+            {
+                item && item.children && item.children.length > 0 
+                    ? <MenuList list={item.children} />
+                    : null
+            }
         </li>
     )
 }
