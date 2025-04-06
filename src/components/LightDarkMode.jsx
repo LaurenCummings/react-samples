05190e8fff31f114d70bea6d4f7452b3db829706
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-04-06 01:37:55 -0400

    Added css

--- src/components/LightDarkMode.jsx
+++ src/components/LightDarkMode.jsx
@@ -9,7 +9,7 @@ function LightDarkMode() {
     }
 
     return (
-        <div className="light-dark-mode">
+        <div className="light-dark-mode" data-theme={theme}>
             <div className="container">
                 <p>Hello World!</p>
                 <button onClick={handleToggleTheme}>Change Theme</button>
