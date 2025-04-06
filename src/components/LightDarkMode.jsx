07cbe4216b2d1d71f694af4b7d5dbca7c83edf85
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-04-06 01:26:17 -0400

    Created function to toggle theme

--- src/components/LightDarkMode.jsx
+++ src/components/LightDarkMode.jsx
@@ -1,11 +1,18 @@
 import '../css/LightDarkMode.css';
+import useLocalStorage from './useLocalStorage';
 
 function LightDarkMode() {
+    const [theme, setTheme] = useLocalStorage('theme', "dark");
+
+    function handleToggleTheme() {
+        setTheme(theme === 'light' ? 'dark' : 'light')
+    }
+
     return (
         <div className="light-dark-mode">
             <div className="container">
                 <p>Hello World!</p>
-                <button>Change Theme</button>
+                <button onClick={handleToggleTheme}>Change Theme</button>
             </div>
         </div>
     )
--- src/components/useLocalStorage.jsx
+++ src/components/useLocalStorage.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function useLocalStorage(key, defaultValue) {
     const [value, setValue] = useState(() => {
