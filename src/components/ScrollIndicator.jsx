71b87213d95571c7f294759d929016da9f181c03
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-04-07 00:39:21 -0400

    Displayed data

--- src/components/ScrollIndicator.jsx
+++ src/components/ScrollIndicator.jsx
@@ -12,7 +12,10 @@ function ScrollIndicator({url}) {
             const response = await fetch(url);
             const data = await response.json();
 
-            console.log(data);
+            if (data && data.products && data.products.length > 0) {
+                setData(data.products);
+                setLoading(false);
+            }
 
         }catch(e) {
             console.log(e);
@@ -26,7 +29,14 @@ function ScrollIndicator({url}) {
 
     return(
         <div>
-
+            <h1>Custom Scroll Indicator</h1>
+            <div className="data-container">
+                {
+                    data && data.length > 0 
+                        ? data.map((dataItem) => <p>{dataItem.title}</p>)
+                        : null
+                }
+            </div>
         </div>
     )
 }
