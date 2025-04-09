20cc62c9e910a2dcdc6dc80953dfba23323818ba
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-04-09 01:53:52 -0400

    Played around with styling

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,8 +2,8 @@ function Modal({ id, header, body, footer, onClose }) {
     return (
         <div id={id || 'Modal'} className="modal">
             <div className="modal-content">
+                <span onClick={onClose} className="close-modal-icon">&times;</span>
                 <div className="modal-header">
-                    <span onClick={onClose} className="close-modal-icon">&times;</span>
                     <h2>{header ? header : 'Header'}</h2>
                 </div>
                 <div className="modal-body">
