4d3e1e56b741144230b5f9d2c2005f492f0ee586
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-04-10 01:28:09 -0400

    Added code to fetch info from github api

--- src/components/GithubProfileFinder.jsx
+++ src/components/GithubProfileFinder.jsx
@@ -1,9 +1,37 @@
 import '../css/GithubProfileFinder.css';
+import { useState, useEffect } from 'react';
 
 function GithubProfileFinder() {
-    return (
-        <div>
+    const [userName, setUserName] = useState('sangammukherjee');
 
+    async function fetchGithubUserData() {
+        const res = await fetch(`https://api.github.com/users/${userName}`)
+
+        const data = await res.json();
+
+        console.log(data);
+    }
+    
+    function handleSubmit() {
+
+    }
+
+    useEffect(() => {
+        fetchGithubUserData()
+    },[])
+
+    return (
+        <div className="github-profile-container">
+            <div className="github-profile-input-wrapper">
+                <input 
+                    name="search-by-username"
+                    type="text"
+                    placeholder="Search Github Username..."
+                    value={userName}
+                    onChange={(event) => setUserName(event.target.value)}
+                />
+                <button onClick={handleSubmit}>Search</button>
+            </div>
         </div>
     )
 }
