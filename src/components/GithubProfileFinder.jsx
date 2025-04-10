03e56bccc3dc7adf33b5679e07e815664e5e4ddc
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-04-10 01:31:37 -0400

    Added loading state and check

--- src/components/GithubProfileFinder.jsx
+++ src/components/GithubProfileFinder.jsx
@@ -3,11 +3,18 @@ import { useState, useEffect } from 'react';
 
 function GithubProfileFinder() {
     const [userName, setUserName] = useState('sangammukherjee');
+    const [userData, setUserData] = useState(null);
+    const [loading, setLoading] = useState(false);
 
     async function fetchGithubUserData() {
+        setLoading(true);
         const res = await fetch(`https://api.github.com/users/${userName}`)
 
         const data = await res.json();
+        if (data) {
+            setUserData(data);
+            setLoading(false);
+        }
 
         console.log(data);
     }
@@ -19,6 +26,10 @@ function GithubProfileFinder() {
     useEffect(() => {
         fetchGithubUserData()
     },[])
+
+    if (loading) {
+        return <h1>Loading data ! Please wait</h1>
+    }
 
     return (
         <div className="github-profile-container">
