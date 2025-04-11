d0603d902d015ec38db344332dafa95595f133fe
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-04-11 02:31:20 -0400

    Set up fetch request

--- src/components/SearchAutocomplete.jsx
+++ src/components/SearchAutocomplete.jsx
@@ -1,8 +1,35 @@
 import '../css/SearchAutocomplete.css';
+import { useState } from 'react';
 
 function SearchAutocomplete() {
+    const [loading, setLoading] = useState(false);
+    const [users, setUsers] = useState([]);
+    const [error, setError] = useState(null);
+
+    async function fetchListOfUsers() {
+        try {
+            setLoading(true);
+            const response = await fetch('https://dummyjson.com/users');
+            const data = await response.json();
+
+            console.log(data);
+
+        } catch(error) {
+            console.log(error)
+            setError(error);
+        }
+    }
+
+    useEffect(() => {
+        fetchListOfUsers()
+    }, [users]);
+
     return (
-        <div>
+        <div className="search-autocomplete-container">
+            <input 
+                name="search-users"
+                placeholder="Search users here..."
+            />
 
         </div>
     )
