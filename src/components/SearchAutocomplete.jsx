5d462df2b5a62aa9d7d07126db1cc92dd0d54293
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-04-11 02:57:07 -0400

    Fixed errors with filtering users

--- src/components/SearchAutocomplete.jsx
+++ src/components/SearchAutocomplete.jsx
@@ -12,14 +12,14 @@ function SearchAutocomplete() {
     function handleChange(event) {
         const query = event.target.value.toLowerCase();
         setSearchParam(query);
-        if (query > 1) {
+        if (query.length > 1) {
             const filteredData = users && users.length 
                 ? users.filter((item) => item.toLowerCase().indexOf(query) > -1)
                 : [];
                 setFilteredUsers(filteredData);
-                showDropdown(true);
+                setShowDropdown(true);
         } else {
-            showDropdown(false);
+            setShowDropdown(false);
         }
     }
 
