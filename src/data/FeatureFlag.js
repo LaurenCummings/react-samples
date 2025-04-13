f05b439f79c3e9d993e77c7f6ce120e3fb8d3861
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-04-13 01:07:40 -0400

    Created FeatureFlag data file

--- src/data/FeatureFlag.js
+++ src/data/FeatureFlag.js
@@ -1,3 +1,16 @@
 const dummyApiResponse = {
-    showLightAndDarkMode : true
+    showLightAndDarkMode : false,
+    showTicTacToeBoard : true,
+    showRandomColorGenerator : true,
+    showAccordian : false,
+    showTreeView : true
 }
+
+function featureFlagsDataServiceCall() {
+    return new Promise((resolve, reject) => {
+        if (dummyApiResponse) setTimeout(resolve(dummyApiResponse), 500);
+        else reject('Some error occurred! Please try again')
+    })
+}
+
+export default featureFlagDataServiceCall;
