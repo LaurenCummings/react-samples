491a37db53ff76ab2f008a72a867e117cbad1d98
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-04-13 02:06:45 -0400

    Added call to dummy api

--- src/contexts/FeatureFlagContext.jsx
+++ src/contexts/FeatureFlagContext.jsx
@@ -1,8 +1,26 @@
-import { createContext } from 'react';
+import { createContext, useState, useEffect } from 'react';
+import featureFlagsDataServiceCall from '../data/FeatureFlag';
 
 export const FeatureFlagContext = createContext(null);
 
 function FeatureFlagGlobalState({ children }) {
+    const [loading, setLoading] = useState(false);
+    const [enabledFlags, setEnabledFlags] = useState({});
+
+    async function fetchFeatureFlags() {
+        try {
+            const response = await featureFlagsDataServiceCall();
+            console.log(response);
+        } catch(error) {
+            console.log(error);
+            throw new Error(error);
+        }
+    }
+
+    useEffect(() => {
+        fetchFeatureFlags();
+    },[]);
+
     return (
         <FeatureFlagContext.Provider value={{}}>
             {children}
