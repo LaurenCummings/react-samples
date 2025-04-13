db92ec2cdee0d715f8f4c8534dd989805d738a5a
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-04-13 02:13:23 -0400

    Added key/values of components to render

--- src/components/FeatureFlag.jsx
+++ src/components/FeatureFlag.jsx
@@ -1,6 +1,30 @@
 import '../css/FeatureFlag.css';
 
 function FeatureFlag() {
+
+    const componentsToRender = [
+        {
+            key : 'showLightAndDarkMode',
+            component : <LightDarkMode />
+        },
+        {
+            key : 'showTicTacToeBoard',
+            component : <TicTacToe />
+        },
+        {
+            key : 'showRandomColorGenerator',
+            component : <ColorGenerator />
+        },
+        {
+            key : 'showAccordian',
+            component : <Accordian />
+        },
+        {
+            key : 'showTreeView',
+            component : <TreeMenu />
+        },
+    ]
+
     return (
         <div>
             <h1>Feature Flags</h1>
