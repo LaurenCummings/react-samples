823501c224a50a6ab5b5f146e0fedf0c15a11718
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-04-14 01:23:00 -0400

    Wrote function to make call to api and get response

--- src/components/UseFetch.jsx
+++ src/components/UseFetch.jsx
@@ -1,9 +1,31 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function useFetch(url, options= {}) {
     const [data, setData] = useState(null);
     const [pending, setPending] = useState(false);
     const [error, setError] = useState(null);
+
+    async function fetchData() {
+        setPending(true);
+        try {
+            const response = await fetch(url, {...options});
+            if (!response.ok) throw new Error(response.statusText);
+
+            const result = await response.json();
+            setData(result);
+            setError(null);
+            setPending(false);
+        } catch(e) {
+            setError(`${e}. Some Error Occurred`);
+            setPending(false);
+        }
+    }
+
+    useEffect(() => {
+
+        fetchData()
+
+    },[url]);
 
     return (
         <div>
