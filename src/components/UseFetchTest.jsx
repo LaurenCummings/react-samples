0f5f26ff3203b58326656f04deed8773ce2136f1
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-04-14 02:11:47 -0400

    Mapped products from useFetch

--- src/components/UseFetchTest.jsx
+++ src/components/UseFetchTest.jsx
@@ -3,11 +3,18 @@ import useFetch from '../components/UseFetch';
 function useFetchTest() {
     const {data, error, pending} = useFetch('https://dummyjson.com/products', {});
 
-    console.log(error, data, pending);
-
     return (
         <div>
             <h1>Use Fetch Hook</h1>
+            {
+                pending ? <h3>Pending ! Please wait</h3> : null
+            }
+            {
+                data && data.products && data.products.length 
+                    ? data.products.map((productItem) => 
+                        <p key={productItem.id}>{productItem.title}</p>)
+                    : null
+            }
         </div>
     )
 }
