3107f7899be00a4ea574bb58efd8407a381eef92
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-04-15 01:33:47 -0400

    Added showContent state

--- src/components/UseOutsideClickTest.jsx
+++ src/components/UseOutsideClickTest.jsx
@@ -1,8 +1,21 @@
+import { useState } from 'react';
 
 function UseOutsideClickTest() {
+    const [showContent, setShowContent] = useState(false);
+
     return (
         <div>
-
+            {showContent ? (
+                <div>
+                    <h1>This is random content</h1>
+                    <p>
+                        Please click outside of this to close this. 
+                        It won't close if you click inside of this content
+                    </p>
+                </div> 
+            ) : (
+                <button onClick={() => setShowContent(true)}>Show Content</button>
+            )}
         </div>
     )
 }
