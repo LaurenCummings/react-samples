0a59702a54bde783f9f12dcf46b71b6c76830777
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-04-15 02:53:19 -0400

    Added code to test component to print width and height

--- src/components/UseWindowResizeTest.jsx
+++ src/components/UseWindowResizeTest.jsx
@@ -1,8 +1,18 @@
+import UseWindowResize from '../components/UseWindowResize';
 
 function UseWindowResizeTest() {
+    const windowSize = UseWindowResize();
+    const { width, height } = windowSize;
+
     return (
         <div>
-
+            <h1>Use Window resize Hook</h1>
+            <p>
+                Width is {width}
+            </p>
+            <p>
+                Height is {height}
+            </p>
         </div>
     )
 }
